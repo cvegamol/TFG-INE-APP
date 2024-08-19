5890a4ef23e2abe4b22c688b9bdf048d4e069df4
cvegamol <cvegmol@gmail.com>
2024-08-19 17:36:24 +0200

    #14 Cambios en la vista de las tablas
    Se han realizado cambios para que el usuario pueda seleccionar de cada una de las variables los valores que desee y los pueda consultar.

--- src/app/(app)/seriesPadron.js
+++ src/app/(app)/seriesPadron.js
@@ -1,52 +1,48 @@
 import React, { useEffect, useState } from 'react';
-import { ScrollView, View, Text } from 'react-native';
+import { ScrollView, View, Text, TouchableOpacity } from 'react-native';
 import { styled } from 'nativewind';
 import Plantilla from '../../components/Plantilla';
 import { useLocalSearchParams, useRouter } from 'expo-router';
 import Loading from '../../components/Loading';
+import { Button, CheckBox } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/Ionicons'; // Asegúrate de tener esta importación
 import {
     widthPercentageToDP as wp,
     heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
+
 const ViewStyled = styled(View);
 const TextStyled = styled(Text);
 const ScrollViewStyled = styled(ScrollView);
+
 const SeriesTabla = () => {
     const router = useRouter();
     const { id, nombre } = useLocalSearchParams();
     const [variables, setVariables] = useState([]);
     const [valoresVariables, setValoresVariables] = useState({});
+    const [selecciones, setSelecciones] = useState({});
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         const obtenerDatos = async () => {
             try {
-
                 const variablesJson = await fetch(`https://servicios.ine.es/wstempus/js/ES/GRUPOS_TABLA/${id}`);
                 if (!variablesJson.ok) {
                     throw new Error('Error al obtener las variables');
                 }
 
                 const variablesText = await variablesJson.text();
-                if (!variablesText) {
-                    throw new Error('Respuesta vacía al obtener las variables');
-                }
-
                 const variablesData = JSON.parse(variablesText);
 
                 const valoresPromises = variablesData.map(async (variable) => {
                     const idv = variable.Id;
 
-                    const valoresJson = await fetch(`https://servicios.ine.es/wstempus/js/ES/VALORES_GRUPOSTABLA/${id}/${idv}`);
+                    const valoresJson = await fetch(`https://servicios.ine.es/wstempus/js/ES/VALORES_GRUPOSTABLA/${id}/${idv}?det=2&tip=A`);
                     if (!valoresJson.ok) {
                         throw new Error(`Error al obtener los valores para la variable ${variable.COD}`);
                     }
 
                     const valoresText = await valoresJson.text();
-                    if (!valoresText) {
-                        throw new Error(`Respuesta vacía al obtener los valores para la variable ${variable.COD}`);
-                    }
-
                     const valoresData = JSON.parse(valoresText);
 
                     return { [variable.Id]: valoresData };
@@ -71,6 +67,41 @@ const SeriesTabla = () => {
         obtenerDatos();
     }, [id]);
 
+    const handleSelectionChange = (variableId, valorId) => {
+        setSelecciones(prevState => {
+            const selectedValues = prevState[variableId] || [];
+            if (selectedValues.includes(valorId)) {
+                return {
+                    ...prevState,
+                    [variableId]: selectedValues.filter(v => v !== valorId)
+                };
+            } else {
+                return {
+                    ...prevState,
+                    [variableId]: [...selectedValues, valorId]
+                };
+            }
+        });
+    };
+
+    const handleSelectAll = (variableId) => {
+        const allValues = valoresVariables[variableId]?.map(valor => valor.Id) || [];
+        setSelecciones(prevState => {
+            const selectedValues = prevState[variableId] || [];
+            const isAllSelected = selectedValues.length === allValues.length;
+
+            return {
+                ...prevState,
+                [variableId]: isAllSelected ? [] : allValues
+            };
+        });
+    };
+
+    const handleConsultar = () => {
+        console.log('Selecciones:', selecciones);
+        // Lógica para consultar los datos basados en las selecciones.
+    };
+
     return (
         <Plantilla>
             <ScrollViewStyled contentContainerStyle={{ flexGrow: 1 }}>
@@ -85,31 +116,60 @@ const SeriesTabla = () => {
                             <TextStyled className="text-lg text-gray-500 mt-2">Cargando...</TextStyled>
                         </ViewStyled>
                     ) : (
-                        variables.map((variable) => (
-                            <ViewStyled
-                                key={variable.Id}
-                                className="mb-6 p-4 bg-white rounded-lg shadow-lg"
-                                style={{ borderBottomWidth: 2, borderBottomColor: '#D1D5DB' }}  // Aplicando estilo directamente
-                            >
-                                <TextStyled className="text-lg font-semibold text-gray-700 mb-2">
-                                    {variable.Nombre}
-                                </TextStyled>
-                                <ScrollViewStyled
-                                    className="max-h-40"
-                                    nestedScrollEnabled={true}
+                        variables.map((variable) => {
+                            const totalValores = valoresVariables[variable.Id]?.length || 0;
+                            const seleccionados = selecciones[variable.Id]?.length || 0;
+                            const allSelected = seleccionados === totalValores && totalValores > 0;
+
+                            return (
+                                <ViewStyled
+                                    key={variable.Id}
+                                    className="mb-6 p-4 bg-white rounded-lg shadow-lg flex-row justify-between"
+                                    style={{ borderBottomWidth: 2, borderBottomColor: '#D1D5DB' }}
                                 >
-                                    {valoresVariables[variable.Id]?.map((valor) => (
-                                        <TextStyled
-                                            key={valor.Id}
-                                            className="text-base text-gray-600 ml-2 py-1"
-                                        >
-                                            {valor.Nombre}
+                                    <ViewStyled className="flex-1">
+                                        <TextStyled className="text-lg font-semibold text-gray-700 mb-2">
+                                            {variable.Nombre}
                                         </TextStyled>
-                                    ))}
-                                </ScrollViewStyled>
-                            </ViewStyled>
-                        ))
+                                        <ScrollViewStyled
+                                            className="max-h-40 mt-2"
+                                            nestedScrollEnabled={true}
+                                        >
+                                            {valoresVariables[variable.Id]?.map((valor) => (
+                                                <ViewStyled key={valor.Id} className="flex-row items-center ">
+                                                    <CheckBox
+                                                        checked={selecciones[variable.Id]?.includes(valor.Id)}
+                                                        onPress={() => handleSelectionChange(variable.Id, valor.Id)}
+                                                    />
+                                                    <TextStyled className="ml-2">{valor.Nombre}</TextStyled> 
+                                                </ViewStyled>
+                                            ))}
+                                        </ScrollViewStyled>
+                                        <ViewStyled className="flex-row justify-between mt-2">
+                                            <TextStyled className="text-sm text-gray-600">
+                                                Seleccionados: {seleccionados}
+                                            </TextStyled>
+                                            <TextStyled className="text-sm text-gray-600">
+                                                Total: {totalValores}
+                                            </TextStyled>
+                                        </ViewStyled>
+                                    </ViewStyled>
+                                    <TouchableOpacity onPress={() => handleSelectAll(variable.Id)}>
+                                        <Icon 
+                                            name={allSelected ? "checkbox-outline" : "square-outline"} 
+                                            size={24} 
+                                            color="gray" 
+                                        />
+                                    </TouchableOpacity>
+                                </ViewStyled>
+                            );
+                        })
                     )}
+
+                    <Button
+                        title="Consultar Selección"
+                        onPress={handleConsultar}
+                    />
                 </ViewStyled>
             </ScrollViewStyled>
         </Plantilla>
