3fb382a35c6b5b0a5df2b7c7db44a3c076ba69ab
cvegamol <cvegmol@gmail.com>
2024-08-23 18:58:57 +0200

    #19 Funcionalidad para compartir un PDF

--- src/app/(app)/datosPadron.js
+++ src/app/(app)/datosPadron.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState, useMemo } from 'react';
-import { ScrollView, View, Text, Dimensions } from 'react-native';
+import { ScrollView, View, Text, Dimensions, Alert } from 'react-native';
 import { styled } from 'nativewind';
 import Plantilla from '../../components/Plantilla';
 import { useLocalSearchParams } from 'expo-router';
-import Loading from '../../components/Loading'; // Asegúrate de tener un componente Loading si no, puedes usar un ActivityIndicator o similar
+import Loading from '../../components/Loading';
+import { Button } from 'react-native-elements';
+import * as Print from 'expo-print';
+import * as FileSystem from 'expo-file-system';
+import { shareAsync } from 'expo-sharing';
 
 const ViewStyled = styled(View);
 const TextStyled = styled(Text);
@@ -12,12 +16,13 @@ import {
     widthPercentageToDP as wp,
     heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
-import { Button } from 'react-native-elements';
+
 const { width } = Dimensions.get('window');
 
 const SeriesTabla = () => {
     const [isLoading, setIsLoading] = useState(true);
     const [datosSeries, setDatosSeries] = useState([]);
+    const [htmlContent, setHtmlContent] = useState('');
     const { tabla, series, periodicidades, valores } = useLocalSearchParams();
 
     const seriesObj = useMemo(() => JSON.parse(series), [series]);
@@ -77,17 +82,57 @@ const SeriesTabla = () => {
             }
         };
 
-        obtenerDatos();
-    }, [seriesObj, periodicidadesObj]);
+        const generateHtmlContent = (datos) => {
+            let tableRows = '';
 
-    if (isLoading) {
-        return (
-            <ViewStyled className="flex-1 justify-center items-center mt-4">
-                <Loading size={50} />
-                <TextStyled className="text-lg text-gray-500 mt-2">Cargando...</TextStyled>
-            </ViewStyled>
-        );
-    }
+            datos.forEach((serieObj) => {
+                let row = `<tr><td style="padding: 8px; border: 1px solid #ddd;">${serieObj.serie}</td>`;
+                serieObj.datos.forEach((datoObj) => {
+                    row += `<td style="padding: 8px; border: 1px solid #ddd; text-align: center;">${datoObj.valor !== 'N/A' ? formatNumero(datoObj.valor) : datoObj.valor}</td>`;
+                });
+                row += '</tr>';
+                tableRows += row;
+            });
+
+            const tableHeaders = Object.keys(periodicidadesObj).map((fechaKey) => {
+                return `<th style="padding: 8px; border: 1px solid #ddd; background-color: #4CAF50; color: white;">${formatFecha(periodicidadesObj[fechaKey].ano, periodicidadesObj[fechaKey].mes, periodicidadesObj[fechaKey].dia)}</th>`;
+            }).join('');
+
+            const html = `
+                <html>
+                    <head>
+                        <style>
+                            table {
+                                width: 100%;
+                                border-collapse: collapse;
+                            }
+                            th, td {
+                                padding: 8px;
+                                border: 1px solid #ddd;
+                                text-align: left;
+                            }
+                        </style>
+                    </head>
+                    <body>
+                        <h2 style="text-align: center;">${tablaObj.Nombre}</h2>
+                        <table>
+                            <tr>
+                                <th style="padding: 8px; border: 1px solid #ddd; background-color: #4CAF50; color: white;">Serie</th>
+                                ${tableHeaders}
+                            </tr>
+                            ${tableRows}
+                        </table>
+                    </body>
+                </html>
+            `;
+
+            setHtmlContent(html);
+        };
+
+        obtenerDatos().then(() => {
+            generateHtmlContent(datosSeries);
+        });
+    }, [seriesObj, periodicidadesObj]);
 
     const formatFecha = (ano, mes, dia) => {
         const meses = [
@@ -108,6 +153,36 @@ const SeriesTabla = () => {
 
     const totalTableWidth = firstColumnWidth + (Object.keys(periodicidadesObj).length * otherColumnFixedWidth) + paddingEnd;
 
+    const generatePDF = async () => {
+        if (!htmlContent) {
+            Alert.alert('Error', 'El contenido del PDF aún no está listo.');
+            return;
+        }
+
+        try {
+            const { uri } = await Print.printToFileAsync({ html: htmlContent });
+            const pdfName = `${FileSystem.documentDirectory}${tablaObj.Nombre}.pdf`;
+            await FileSystem.moveAsync({
+                from: uri,
+                to: pdfName,
+            });
+            await shareAsync(pdfName);
+            Alert.alert('PDF guardado', `PDF guardado en: ${pdfName}`);
+        } catch (error) {
+            console.error('Error al generar PDF:', error);
+            Alert.alert('Error', 'Hubo un problema al generar el PDF');
+        }
+    };
+
+    if (isLoading) {
+        return (
+            <ViewStyled className="flex-1 justify-center items-center mt-4">
+                <Loading size={50} />
+                <TextStyled className="text-lg text-gray-500 mt-2">Cargando...</TextStyled>
+            </ViewStyled>
+        );
+    }
+
     return (
         <Plantilla>
             <ViewStyled className="p-4 bg-gray-100 rounded-lg m-4 shadow-md">
@@ -122,11 +197,11 @@ const SeriesTabla = () => {
                             <TextStyled style={{
                                 width: firstColumnWidth,
                                 fontWeight: 'bold',
-                                backgroundColor: '#4CAF50', // Color de fondo para el encabezado
+                                backgroundColor: '#4CAF50',
                                 color: '#fff',
                                 borderWidth: 1,
                                 borderColor: '#ccc',
-                                padding: 10,  // Ajuste de padding
+                                padding: 10,
                             }}>
                                 Serie
                             </TextStyled>
@@ -135,11 +210,11 @@ const SeriesTabla = () => {
                                     width: otherColumnFixedWidth,
                                     fontWeight: 'bold',
                                     textAlign: 'center',
-                                    backgroundColor: '#4CAF50', // Color de fondo para el encabezado
+                                    backgroundColor: '#4CAF50',
                                     color: '#fff',
                                     borderWidth: 1,
                                     borderColor: '#ccc',
-                                    padding: 10,  // Ajuste de padding
+                                    padding: 10,
                                 }}>
                                     {formatFecha(periodicidadesObj[fechaKey].ano, periodicidadesObj[fechaKey].mes, periodicidadesObj[fechaKey].dia)}
                                 </TextStyled>
@@ -153,7 +228,7 @@ const SeriesTabla = () => {
                                     fontWeight: 'bold',
                                     borderWidth: 1,
                                     borderColor: '#ccc',
-                                    padding: 10,  // Ajuste de padding
+                                    padding: 10,
                                 }}>
                                     {serieObj.serie}
                                 </TextStyled>
@@ -163,7 +238,7 @@ const SeriesTabla = () => {
                                         textAlign: 'center',
                                         borderWidth: 1,
                                         borderColor: '#ccc',
-                                        padding: 10,  // Ajuste de padding
+                                        padding: 10,
                                     }}>
                                         {datoObj.valor !== 'N/A' ? formatNumero(datoObj.valor) : datoObj.valor}
                                     </TextStyled>
@@ -173,7 +248,7 @@ const SeriesTabla = () => {
                     </ViewStyled>
                 </ScrollViewStyled>
             </ScrollViewStyled>
-            <Button title="Descargar PDF" />
+            <Button title="Compartir PDF" onPress={generatePDF} />
         </Plantilla>
     );
 };
