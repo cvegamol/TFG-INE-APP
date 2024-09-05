b72cbafa649a5601ae39eebb2d512f313a65b5c8
cvegamol <cvegmol@gmail.com>
2024-09-05 10:09:41 +0200

    #45 Modificacion componente StackedBarChart
    Se ha modificado varias propiedades del componente. La primera modificación que se ha realizado es que la gráfica tenga un scrollvoew horizontal esto hacce que sea responsive.

--- src/components/graph/stackedBarChart/StackedBarChart.js
+++ src/components/graph/stackedBarChart/StackedBarChart.js
@@ -1,18 +1,20 @@
 import React from "react";
-import { View } from "react-native";
+import { View, ScrollView } from "react-native";
 import { G, Rect, Svg, Text } from "react-native-svg";
-
 import AbstractChart from "../AbstractChart";
 
 class StackedBarChart extends AbstractChart {
   static defaultProps = {
-    onDataPointClick: () => { }, // Añade un valor predeterminado para onDataPointClick
-    formatXLabel: (xLabel) => xLabel
+    onDataPointClick: () => { },
+    formatXLabel: (xLabel) => xLabel,
   };
 
-  getBarPercentage = () => {
-    const { barPercentage = 1 } = this.props.chartConfig;
-    return barPercentage;
+  // Ajustamos el ancho de las barras para que sean más finas
+  getDynamicBarWidth = (dataLength, chartWidth, paddingRight) => {
+    const availableBarSpace = chartWidth - paddingRight * 2;
+    const maxBarWidth = availableBarSpace / dataLength;
+    const minBarWidth = 10; // Definimos un ancho mínimo para las barras
+    return Math.max(minBarWidth, Math.min(maxBarWidth, 40)); // Aumentamos el ancho máximo permitido a 40
   };
 
   getBarRadius = (ret, x) => {
@@ -30,34 +32,27 @@ class StackedBarChart extends AbstractChart {
     border,
     colors,
     stackedBar = false,
-    verticalLabelsHeightPercentage
-  }) =>
-    data.map((x, i) => {
-      const barWidth = 32 * this.getBarPercentage();
-      const ret = [];
+    verticalLabelsHeightPercentage,
+  }) => {
+    const barWidth = this.getDynamicBarWidth(data.length, width, paddingRight);
+    const ret = [];
+
+    return data.map((x, i) => {
       let h = 0;
       let st = paddingTop;
-
-      let fac = 1;
-      if (stackedBar) {
-        fac = 0.7;
-      }
-      const sum = this.props.percentile ? x.reduce((a, b) => a + b, 0) : border;
       const barsAreaHeight = height * verticalLabelsHeightPercentage;
+
+      const sum = this.props.percentile ? x.reduce((a, b) => a + b, 0) : border;
 
       for (let z = 0; z < x.length; z++) {
         h = barsAreaHeight * (x[z] / sum);
         const y = barsAreaHeight - h + st;
-        const xC =
-          (paddingRight +
-            (i * (width - paddingRight)) / data.length +
-            barWidth / 2) *
-          fac;
+        const xC = paddingRight + (i * (width - paddingRight)) / data.length + barWidth / 2;
 
         ret.push(
           <Rect
             key={Math.random()}
-            x={xC}
+            x={xC - barWidth / 2} // Ajustamos para centrar las barras
             y={y}
             rx={this.getBarRadius(ret, x)}
             ry={this.getBarRadius(ret, x)}
@@ -65,47 +60,25 @@ class StackedBarChart extends AbstractChart {
             height={h}
             fill={colors[z]}
             onPressIn={() => {
-              // Llamada a la función onDataPointClick al hacer clic en una barra
               this.props.onDataPointClick({
                 index: i,
                 value: x[z],
                 datasetIndex: z,
                 label: this.props.data.labels[i],
-                datasetLabel: this.props.data.legend[z]
+                datasetLabel: this.props.data.legend[z],
               });
             }}
           />
         );
-
-        // Comentamos o eliminamos el bloque que muestra el valor sobre la barra
-        /*
-        if (!this.props.hideLegend) {
-          ret.push(
-            <Text
-              key={Math.random()}
-              x={xC + 7 + barWidth / 2}
-              textAnchor="end"
-              y={h > 15 ? y + 15 : y + 7}
-              {...this.getPropsForLabels()}
-            >
-              {x[z]}
-            </Text>
-          );
-        }
-        */
 
         st -= h;
       }
 
       return ret;
     });
+  };
 
-  renderLegend = ({
-    legend,
-    colors,
-    width,
-    height
-  }) =>
+  renderLegend = ({ legend, colors, width, height }) =>
     legend.map((x, i) => {
       return (
         <G key={Math.random()}>
@@ -131,11 +104,11 @@ class StackedBarChart extends AbstractChart {
 
   render() {
     const paddingTop = 15;
-    const paddingRight = 50;
-    const barWidth = 32 * this.getBarPercentage();
+    const paddingRight = 30; // Ajustamos para reducir el margen lateral
+    const extraRightPadding = 30; // Añadimos este margen adicional al final para evitar corte
 
     const {
-      width,
+      width, // Ancho pasado como prop
       height,
       style = {},
       data,
@@ -145,21 +118,15 @@ class StackedBarChart extends AbstractChart {
       decimalPlaces,
       percentile = false,
       verticalLabelsHeightPercentage = 0.75,
-      formatYLabel = (yLabel) => {
-        return yLabel;
-      },
+      formatYLabel = (yLabel) => yLabel,
       formatXLabel = (xLabel) => xLabel,
-      hideLegend = false
+      hideLegend = false,
     } = this.props;
 
     const { borderRadius = 0 } = style;
-    const config = {
-      width,
-      height
-    };
-
     let border = 0;
 
+    // Calcular el valor máximo de la suma de las barras
     let max = 0;
     for (let i = 0; i < data.data.length; i++) {
       const actual = data.data[i].reduce((pv, cv) => pv + cv, 0);
@@ -168,86 +135,94 @@ class StackedBarChart extends AbstractChart {
       }
     }
 
-    if (percentile) {
-      border = 100;
-    } else {
-      border = max;
-    }
+    border = max;
 
-    const showLegend = !hideLegend && data.legend && data.legend.length != 0;
+    const showLegend = !hideLegend && data.legend && data.legend.length !== 0;
     const stackedBar = showLegend;
 
+    // Cálculo dinámico para que sea responsive y permita espacio para el último valor
+    const calculatedWidth = Math.max(width, data.labels.length * 60) + extraRightPadding;
+
     return (
-      <View style={style}>
-        <Svg height={height} width={width}>
-          {this.renderDefs({
-            ...config,
-            ...this.props.chartConfig
-          })}
-          <Rect
-            width="100%"
-            height={height}
-            rx={borderRadius}
-            ry={borderRadius}
-            fill="url(#backgroundGradient)"
-          />
-          <G>
-            {this.renderHorizontalLines({
-              ...config,
-              count: segments,
-              paddingTop,
-              verticalLabelsHeightPercentage
+      <ScrollView horizontal={true}>
+        <View style={style}>
+          <Svg height={height} width={calculatedWidth}>
+            {this.renderDefs({
+              width: calculatedWidth,
+              height,
+              ...this.props.chartConfig,
             })}
-          </G>
-          <G>
-            {withHorizontalLabels
-              ? this.renderHorizontalLabels({
-                ...config,
+            <Rect
+              width="100%"
+              height={height}
+              rx={borderRadius}
+              ry={borderRadius}
+              fill="url(#backgroundGradient)"
+            />
+
+            <G>
+              {this.renderHorizontalLines({
+                width: calculatedWidth,
+                height,
                 count: segments,
-                data: [0, border],
+                paddingTop,
+                verticalLabelsHeightPercentage,
+              })}
+            </G>
+            <G>
+              {withHorizontalLabels
+                ? this.renderHorizontalLabels({
+                  width: calculatedWidth,
+                  height,
+                  count: segments,
+                  data: [0, border],
+                  paddingTop,
+                  paddingRight,
+                  decimalPlaces,
+                  verticalLabelsHeightPercentage,
+                  formatYLabel,
+                })
+                : null}
+            </G>
+            <G>
+              {withVerticalLabels
+                ? this.renderVerticalLabels({
+                  width: calculatedWidth,
+                  height,
+                  labels: data.labels,
+                  paddingRight: paddingRight + 12,
+                  stackedBar,
+                  paddingTop,
+                  horizontalOffset: (this.getDynamicBarWidth(data.labels.length, calculatedWidth, paddingRight) / 2),
+                  verticalLabelRotation: 270, // Rotación de 270 grados
+                  verticalLabelsHeightPercentage,
+                  formatXLabel,
+                })
+                : null}
+            </G>
+            <G>
+              {this.renderBars({
+                width: calculatedWidth,
+                height,
+                data: data.data,
+                border,
+                colors: this.props.data.barColors,
                 paddingTop,
                 paddingRight,
-                decimalPlaces,
-                verticalLabelsHeightPercentage,
-                formatYLabel
-              })
-              : null}
-          </G>
-          <G>
-            {withVerticalLabels
-              ? this.renderVerticalLabels({
-                ...config,
-                labels: data.labels,
-                paddingRight: paddingRight + 28,
                 stackedBar,
-                paddingTop,
-                horizontalOffset: barWidth,
-                verticalLabelRotation: 270,  // Rotación de 270 grados
                 verticalLabelsHeightPercentage,
-                formatXLabel
-              })
-              : null}
-          </G>
-          <G>
-            {this.renderBars({
-              ...config,
-              data: data.data,
-              border,
-              colors: this.props.data.barColors,
-              paddingTop,
-              paddingRight: paddingRight + 20,
-              stackedBar,
-              verticalLabelsHeightPercentage
-            })}
-          </G>
-          {showLegend &&
-            this.renderLegend({
-              ...config,
-              legend: data.legend,
-              colors: this.props.data.barColors
-            })}
-        </Svg>
-      </View>
+              })}
+            </G>
+            {showLegend &&
+              this.renderLegend({
+                width: calculatedWidth,
+                height,
+                legend: data.legend,
+                colors: this.props.data.barColors,
+              })}
+          </Svg>
+        </View>
+      </ScrollView>
     );
   }
 }
