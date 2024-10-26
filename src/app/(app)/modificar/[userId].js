4d734e243952e99e2946386ab8a18b9df93e5352
cvegamol <cvegmol@gmail.com>
2024-10-26 17:40:02 +0200

    #108 Cambio en el estilo del componente que modifica un usuario

--- src/app/(app)/modificar/[userId].js
+++ src/app/(app)/modificar/[userId].js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { ScrollView, View, Text, Alert, TouchableOpacity, TextInput } from 'react-native';
 import { styled } from 'nativewind';
-import { useLocalSearchParams, useRouter, useSearchParams } from 'expo-router';
+import { useLocalSearchParams, useRouter } from 'expo-router';
 import { doc, getDoc, updateDoc } from 'firebase/firestore';
 import { useAuth } from '../../../context/authContext';
 import { FontAwesome6 } from '@expo/vector-icons';
@@ -64,7 +64,7 @@ const ModifyUser = () => {
           return (
                <ViewStyled className="flex-1 justify-center items-center">
                     <Loading size={50} />
-                    <TextStyled className="text-lg text-gray-500 mt-2">Cargando...</TextStyled>
+                    <TextStyled className="text-lg text-teal-700 mt-2">Cargando...</TextStyled>
                </ViewStyled>
           );
      }
@@ -72,59 +72,59 @@ const ModifyUser = () => {
      return (
           <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
                <ViewStyled className="flex-1 justify-center items-center p-4">
-                    <ViewStyled className="bg-white/40 rounded-2xl p-6 mx-auto w-full max-w-md backdrop-blur-xl border-2 border-white">
+                    <ViewStyled className="bg-teal-50 rounded-2xl p-6 mx-auto w-full max-w-md shadow-lg border-2 border-teal-600">
                          <ViewStyled className="flex-1 justify-center space-y-6">
                               <ViewStyled className="space-y-6 mx-auto w-full max-w-md">
-                                   <TextStyled className="font-bold tracking-wider text-center text-neutral-800 text-2xl">
-                                        Editar Perfil del Usuario
+                                   <TextStyled className="font-bold tracking-wider text-center text-teal-800 text-2xl">
+                                        Editar Usuario
                                    </TextStyled>
 
                                    {/* Input de Nombre */}
-                                   <ViewStyled className="flex-row gap-3 px-4 bg-gray-200 items-center rounded-2xl mx-auto w-full max-w-md">
-                                        <FontAwesome6 name="user" size={20} color="gray" />
+                                   <ViewStyled className="flex-row gap-3 px-4 bg-teal-200 items-center rounded-2xl mx-auto w-full max-w-md">
+                                        <FontAwesome6 name="user" size={20} color="#0c4a4e" />
                                         <TextStyledInput
                                              onChangeText={(value) => handleChange('name', value)}
                                              value={user.name}
                                              style={{ flex: 1 }}
-                                             className="font-medium text-neutral-800"
+                                             className="font-medium text-teal-900"
                                              placeholder="Nombre"
-                                             placeholderTextColor="#172554"
+                                             placeholderTextColor="#064e3b"
                                         />
                                    </ViewStyled>
 
                                    {/* Input de Apellido */}
-                                   <ViewStyled className="flex-row gap-3 px-4 bg-gray-200 items-center rounded-2xl mx-auto w-full max-w-md">
-                                        <FontAwesome6 name="user" size={20} color="gray" />
+                                   <ViewStyled className="flex-row gap-3 px-4 bg-teal-200 items-center rounded-2xl mx-auto w-full max-w-md">
+                                        <FontAwesome6 name="user" size={20} color="#0c4a4e" />
                                         <TextStyledInput
                                              onChangeText={(value) => handleChange('surname', value)}
                                              value={user.surname}
                                              style={{ flex: 1 }}
-                                             className="font-medium text-neutral-800"
+                                             className="font-medium text-teal-900"
                                              placeholder="Apellido"
-                                             placeholderTextColor="#172554"
+                                             placeholderTextColor="#064e3b"
                                         />
                                    </ViewStyled>
 
                                    {/* Input de Correo Electrónico */}
-                                   <ViewStyled className="flex-row gap-3 px-4 bg-gray-200 items-center rounded-2xl mx-auto w-full max-w-md">
-                                        <FontAwesome6 name="envelope" size={20} color="gray" />
+                                   <ViewStyled className="flex-row gap-3 px-4 bg-teal-200 items-center rounded-2xl mx-auto w-full max-w-md">
+                                        <FontAwesome6 name="envelope" size={20} color="#0c4a4e" />
                                         <TextStyledInput
                                              value={user.email}
                                              style={{ flex: 1 }}
-                                             className="font-medium text-neutral-800"
+                                             className="font-medium text-teal-900"
                                              placeholder="Correo Electrónico"
-                                             placeholderTextColor="#172554"
+                                             placeholderTextColor="#064e3b"
                                              editable={false}
                                         />
                                    </ViewStyled>
 
                                    {/* Selector de Rol */}
-                                   <ViewStyled className="flex-row gap-3 px-4 bg-gray-200 items-center rounded-2xl mx-auto w-full max-w-md">
-                                        <FontAwesome6 name="user-shield" size={20} color="gray" />
+                                   <ViewStyled className="flex-row gap-3 px-4 bg-teal-200 items-center rounded-2xl mx-auto w-full max-w-md">
+                                        <FontAwesome6 name="user-shield" size={20} color="#0c4a4e" />
                                         <Picker
                                              selectedValue={user.rol}
                                              onValueChange={(value) => handleChange('rol', value)}
-                                             style={{ flex: 1 }}
+                                             style={{ flex: 1, color: '#0c4a4e' }}
                                         >
                                              <Picker.Item label="General" value="general" />
                                              <Picker.Item label="Admin" value="admin" />
@@ -135,12 +135,12 @@ const ModifyUser = () => {
                                    <ViewStyled>
                                         {loading ? (
                                              <ViewStyled className="flex-row justify-center">
-                                                  <TextStyled>Cargando...</TextStyled>
+                                                  <TextStyled className="text-teal-700">Cargando...</TextStyled>
                                              </ViewStyled>
                                         ) : (
                                              <StyledTouchableOpacity
                                                   onPress={handleUpdateUser}
-                                                  className="bg-stone-800 rounded-xl justify-center items-center mx-auto w-full max-w-md p-3"
+                                                  className="bg-teal-700 rounded-xl justify-center items-center mx-auto w-full max-w-md p-3"
                                              >
                                                   <TextStyled className="text-white font-bold tracking-wider">
                                                        Guardar Cambios
